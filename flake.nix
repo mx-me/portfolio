8e83827fcf94cc517248597bb3bb9688e5a817f4
mx-me <116444282+mx-me@users.noreply.github.com>
2025-12-02 02:27:18 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,35 @@
 {
-  description = "Zola flake";
+  description = "A flake for developing and building my personal website";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    devShells = builtins.mapAttrs (system: pkgs: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          zola
-        ];
-
-      };
-    }) nixpkgs.legacyPackages;
-  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.website = pkgs.stdenv.mkDerivation rec {
+          pname = "static-website";
+          version = "2021-11-19";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.zola ];
+          buildPhase = "zola build";
+          installPhase = "cp -r public $out";
+        };
+        defaultPackage = self.packages.${system}.website;
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            zola
+          ];
+        };
+      }
+    );
 }
